f9f69f652a3529a6e2f7dbf82a59f3fbca087377
atk-titan <gauravmane8451@gmail.com>
2025-09-22 23:17:56 +0530

    Redis Lists

--- Assignment/week15/redis_learning/index.js
+++ Assignment/week15/redis_learning/index.js
@@ -11,7 +11,16 @@ async function init(){
     setTimeout(async ()=>{
         res = await client.get("user:3");
         console.log(res);
-    }, 4000);
+    }, 4401);
+
+    // let unreadMessages = "messages:unread";
+    // let res3 = await client.lpush(unreadMessages,['hii how are you',"what are you doing","tell me about yourself"]);
+
+    // res3 = await client.lrange(unreadMessages,0,-2);
+
+    // console.log(res3);
+
+    return;
 }
 
 init();

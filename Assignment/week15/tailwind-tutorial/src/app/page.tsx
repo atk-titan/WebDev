714c3651f21485ebd59d8339f2563eb2965d32cb
atk-titan <gauravmane8451@gmail.com>
2025-10-12 23:54:07 +0530

    --

--- Assignment/week15/tailwind-tutorial/src/app/page.tsx
+++ Assignment/week15/tailwind-tutorial/src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 export default function Home() {
   return (
     <div className="relative flex h-full justify-center overflow-hidden bg-neutral-900">
-      <div className="from-primary absolute inset-0 h-40 w-full bg-[radial-gradient(circle_at_center,var(--tw-gradient-from)_0%,transparent_70%)] to-transparent opacity-40 blur-3xl -top-20" />
+      <div className="from-primary absolute inset-0 -top-20 h-40 w-full bg-[radial-gradient(circle_at_center,var(--tw-gradient-from)_0%,transparent_70%)] to-transparent opacity-40 blur-3xl" />
       <Hero />
     </div>
   );

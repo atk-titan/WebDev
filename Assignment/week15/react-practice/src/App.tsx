43fbbc735159b419e227483afcfff66f0d47fa7b
atk-titan <gauravmane8451@gmail.com>
2025-10-23 23:52:01 +0530

    custom cursor component

--- Assignment/week15/react-practice/src/App.tsx
+++ Assignment/week15/react-practice/src/App.tsx
@@ -1,8 +1,10 @@
+import CustomCursor from "./Components/CustomCursor";
 import Keybord from "./Components/Keybord";
 
 const App = () => {
   return (
     <div className="bg-background text-foreground h-screen w-screen flex items-center justify-center">
+      <CustomCursor />
       <Keybord />
     </div>
   );
---
+++ Assignment/week15/react-practice/src/Components/CustomCursor.tsx
@@ -0,0 +1,16 @@
+const CustomCursor = () => {
+  return (
+    <div className="absolute z-50 inset-0">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+      >
+        <path className="fill-foreground" d="M4.5.79v22.42l6.56-6.57h9.29L4.5.79z"></path>
+      </svg>
+    </div>
+  );
+};
+
+export default CustomCursor;
